42e393b338c47f16d5d2cf4c8d6e978a92cc5183
jannyrsc <jannyraissa15@gmail.com>
2025-11-21 00:01:09 -0300

    condições simples e compostas

---
+++ aula04/ex001.js
@@ -0,0 +1,15 @@
+var vel = 58.2
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){ // condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança')
+
+
+var pais = 'EUA'
+console.log(`Vivendo em ${pais} `)
+if (pais == 'Brasil'){ // condição composta
+    console.log('Você é Brasileiro')
+}else{
+console.log('Você é estrangeiro')
+}
