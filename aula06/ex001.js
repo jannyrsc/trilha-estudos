cc4afa361d39ec0e700ead7e25943f5c52c9f6c7
jannyrsc <jannyraissa@gmail.com>
2025-11-24 09:15:52 -0300

    repetições em js

---
+++ aula06/ex001.js
@@ -0,0 +1,6 @@
+var fatia = 8
+    while(fatia>0){
+        console.log("Ainda tem pizza")
+        fatia--;
+    }
+    console.log("A pizza acabou")
---
+++ aula06/ex003.js
@@ -0,0 +1,9 @@
+var fatia = 8
+function comerPizza(){
+    while(fatia>0){
+        fatia--;
+        console.log("Fatias restantes: " +fatia)
+    }
+    console.log("Acabou a pizza!")
+}
+comerPizza()
